befde11919d33dbdae6f550f63b99e9624db1099
Tarboeuf <5155564+Tarboeuf@users.noreply.github.com>
2023-12-09 16:33:23 +0100

    day9

---
+++ AdventOfCode2023/Day9/Day.cs
@@ -0,0 +1,72 @@
+﻿using AdventOfCode.Lib;
+
+namespace AdventOfCode2023.Day9
+{
+
+    [Day(ExpectedValue = "114")]
+    public class Puzzle1 : IDay
+    {
+        public string GetPuzzle(string input, bool isRealCase)
+        {
+            return input.GetLines()
+                .Select(l => l.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(v => v.ToInt()))
+                .Select(GetNextValue)
+                .Sum().ToString();
+        }
+
+        private int GetNextValue(IEnumerable<int> values)
+        {
+            List<int> currentValues = values.ToList();
+            List<int> lastValues = new List<int>();
+            while (true)
+            {
+                lastValues.Add(currentValues.Last());
+                currentValues = currentValues.Zip(currentValues.Skip(1))
+                    .Select(v => v.Second - v.First)
+                    .ToList();
+                if (currentValues.Distinct().Count() == 1)
+                {
+                    lastValues.Add(currentValues.Last());
+                    return lastValues.Sum();
+                }
+            }
+        }
+    }
+
+    [Day(ExpectedValue = "2")]
+    public class Puzzle2 : IDay
+    {
+        public string GetPuzzle(string input, bool isRealCase)
+        {
+            return input.GetLines()
+                .Select(l => l.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(v => v.ToInt()))
+                .Select(GetPreviousValue)
+                .Sum().ToString();
+        }
+
+        private int GetPreviousValue(IEnumerable<int> values)
+        {
+            List<int> currentValues = values.ToList();
+            List<int> firstValues = new List<int>();
+            while (true)
+            {
+                firstValues.Add(currentValues.First());
+                currentValues = currentValues.Zip(currentValues.Skip(1))
+                    .Select(v => v.Second - v.First)
+                    .ToList();
+                if (currentValues.Distinct().Count() == 1)
+                {
+                    firstValues.Reverse();
+
+                    int current  = currentValues.First();
+                    foreach (var firstValue in firstValues)
+                    {
+                        current = firstValue - current;
+                    }
+
+                    return current;
+                }
+            }
+        }
+    }
+}
